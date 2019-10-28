3444543f7dc6e59fe9c6ebff6aaba0c435dcac87
Christian-Zwinkels-CAS <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-10-28 18:36:54 +0100

    Added basic data and a feed forward loop
    Feedforward loop does a random prediction on a random sample from the data. All the layers are stored in a list but only the last element (with the activation function applied gets printed). The data example is a XOR gate.

--- Supervised_Learning/Backpropagation/Basic_Example.py
+++ Supervised_Learning/Backpropagation/Basic_Example.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 
+# Input data
+data_in = np.array([[0, 0],
+                    [0, 1],
+                    [1, 0],
+                    [1, 1]])
+
+# The expected outputs
+data_out = np.array([0, 1, 1, 0])
+
 e = 2.7182818284  # Just in case np.exp gives me an error
 
 
@@ -9,7 +18,7 @@ def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
 
-layer_sizes = (2, 3, 2)
+layer_sizes = (2, 3, 1)
 
 # Creates tuple that represent the weight matirices sizes
 weight_sizes = [(y, x) for y, x in zip(layer_sizes[1:], layer_sizes[:-1])]
@@ -20,4 +29,19 @@ weights = [np.random.standard_normal(s) for s in weight_sizes]
 # Biases start out as zeros
 biases = [np.zeros((s, 1)) for s in layer_sizes[1:]]
 
-print(weights)
+# Chooses a random interger
+ri = numpy.random.randint(len(data_in))
+
+# Sets the activation layer to a random sample from the input data
+a = data_in[ri].reshape((layer_sizes[0], 1))
+
+# Feedforward
+layers = []  # A list to hold the values of the layers
+z = a
+for w, b in zip(weights, biases):
+    z = np.dot(w, z) + b
+    layers.append(z)
+layers.insert(0, a)  # Inserts the activation layer
+layers_sigmoid = [sigmoid(sig) for sig in layers]  # Applies sigmoid to them
+
+print(layers_sigmoid[-1])
