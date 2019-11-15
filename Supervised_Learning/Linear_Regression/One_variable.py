df178cd5409defb2bd3e26f690891eb5cd2128ef
Christian Zwinkels-Valero <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-11-15 19:13:23 +0100

    Hypothesis and cost function
    Added a hypothesis function and a function that calculates the average cost of all the training examples. Plotted the data and the prediction which is currently using random parameters.

--- Supervised_Learning/Linear_Regression/One_variable.py
+++ Supervised_Learning/Linear_Regression/One_variable.py
@@ -8,7 +8,7 @@ import numpy as np
 import csv
 from matplotlib import pyplot as plt
 
-# Processing the csv
+# Processing the data
 a = open("data.csv")
 b = [row for row in csv.reader(a)]
 c = [[b[i][-1]] for i in range(len(b))]
@@ -21,3 +21,30 @@ y = np.array(c, dtype=np.float32)
 # Plotting the data
 plt.scatter(X, y)
 plt.show
+
+# Hypothesis function
+theta = np.random.standard_normal((1, 2))
+
+
+def hypothesis(data):
+    hyp = []
+    for x in data:
+        pred = theta[0][0] + (x * theta[0][1])
+        hyp.append(pred)
+    return hyp
+
+
+# Error
+def cost(prediction, y):
+    err = []
+    for p, y in zip(prediction, y):
+        cost = (p - y)**2
+        err.append(cost)
+    return sum(err) / len(err)
+
+
+# Test run of the prediction and plotting the results (currently random)
+prediction = hypothesis(X)
+print(cost(prediction, y))
+
+plt.scatter(prediction, y)
