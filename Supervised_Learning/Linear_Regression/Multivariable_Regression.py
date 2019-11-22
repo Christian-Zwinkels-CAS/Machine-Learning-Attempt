3b7708060fc2b32c360a44531f04fac01a802e33
Christian Zwinkels-Valero <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-11-22 20:50:17 +0100

    Added basic feature scaling
    This feature scaling divides each row of the Xs and y (excepted teh first X row) by its maximum value in order to have values between 0 and 1 which makes everything faster. The hypothesis takes into consideration when the data is scaled back.

--- Supervised_Learning/Linear_Regression/Multivariable_Regression.py
+++ Supervised_Learning/Linear_Regression/Multivariable_Regression.py
@@ -12,14 +12,30 @@ import numpy as np
 def openCsv(file):
     a = open(file)
     b = [row for row in csv.reader(a)]
-    for i in range(len(b)):
-        b[i].insert(0, 1)
     c = [[b[i][-1]] for i in range(len(b))]
     for i in range(len(b)):
         del b[i][-1]
+        b[i].insert(0, 1)
     X = np.array(b, dtype=np.float64)
     y = np.array(c, dtype=np.float64)
     return X, y
 
 
+# Applying basic feature scaling
 X, y = openCsv("Data_2.csv")
+X_max = []
+for i in range(1, np.size(X, axis=1)):
+    if i == 1:
+        y_max = np.max(y)
+        y /= y_max
+
+    X_max.append(np.max(X[:, i]))
+    X[:, i] /= X_max[i - 1]
+
+thetas = np.abs(np.random.standard_normal((X.shape[-1], 1)))
+
+
+def hypothethis(data_in):
+    data_in / X_max
+    pred = (np.dot(thetas.T, data_in)) * y_max
+    return pred
