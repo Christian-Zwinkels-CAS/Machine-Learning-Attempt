e7d10c26e9608d67d5e6616dd9588430610f0a75
Christian Zwinkels-Valero <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-12-11 20:53:18 +0100

    Hypothesis
    Added a hypothesis fucntion, but since the values of the features are high then the output is most of the time 1 so I need to apply feature scaling.

--- Supervised_Learning/Logistic_Regression/Logistic_regression.py
+++ Supervised_Learning/Logistic_Regression/Logistic_regression.py
@@ -8,12 +8,18 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
+# Logistic function
+def sigmoid(x):
+    return 1 / (1 + np.exp(x))
+
+
 # Importing the data
 data = pd.read_csv("Admission_Predict.csv", header=None)
 X = data[[0, 1]].to_numpy()
 X = X.astype(np.float32)
-plot_1 = X[:, 0]
-plot_2 = X[:, 1]
+X = np.insert(X, 0, 1, axis=1)
+plot_1 = X[:, 1]
+plot_2 = X[:, 2]
 y = data[2].to_numpy()
 y = y.astype(np.float32())
 
@@ -28,3 +34,10 @@ for i in np.unique(y):
 plt.xlabel("GRE Score")
 plt.ylabel("CGPA")
 plt.show()
+
+# Hypothesis
+thetas = np.random.standard_normal((X.shape[1], 1))
+
+def hypothesis(data_in, parameters):
+    p = np.dot(parameters.T, data_in.reshape((len(data_in), 1)))
+    return sigmoid(p)
