3b6c9ab858f62707597c1bb2d734775ce3b0abbb
Christian Zwinkels-Valero <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2020-01-05 19:17:47 +0100

    Deltas for hidden layers
    Calculation for the deltas for the hidden layers, trying to figure out which way is best to compute the changes to the weights.

--- Supervised_Learning/Neural_Network/NN.py
+++ Supervised_Learning/Neural_Network/NN.py
@@ -37,19 +37,21 @@ y = np.array([data[data.shape[-1] - 1].to_numpy()])
 # Initialization
 layer_sizes = (X.shape[0], 4, 3, 2, y.shape[0])
 weight_sizes = [(y, x) for y, x in zip(layer_sizes[1:], layer_sizes[0:])]
-weights = [np.random.standard_normal(l)*np.sqrt(1/l[-1]) for l in weight_sizes]
+weights = [np.random.rand(l[0], l[1])*np.sqrt(1/l[1]) for l in weight_sizes]
 biases = [np.zeros((i, 1)) for i in layer_sizes[1:]]
 
 # Foward propagation
 def feedforward(data_in, Ws, Bs):
     Z = []
     A = [data_in]  # First activation layer is the inputs
+
     # Hidden layer computation
     for i in range(len(Ws) - 1):
         z = np.dot(Ws[i], A[-1]) + Bs[i]
         a = relu(z, d=False)
         Z.append(z)
         A.append(a)
+
     # Ouput layer computation
     z = np.dot(Ws[-1], A[-1]) + Bs[-1]
     Z.append(z)
@@ -62,13 +64,23 @@ def feedforward(data_in, Ws, Bs):
 def costs(data_in, outputs, Ws, Bs):
     Z, pred = feedforward(data_in, Ws, Bs)
     delta = []
+    dj_dw = []
+
     # Loss computation
     loss = -1*(outputs*np.log(pred[-1]) + (1-outputs)*np.log(1 - pred[-1]))
     loss = np.mean(loss)
+
     # Final layer derivatives
-    dj_da = (pred[-1]-outputs) / (2*pred[-1]*(pred[-1] - 1))
+    dj_da = -1*(pred[-1] - outputs)/((pred[-1] - 1)*pred[-1])
     da_dz = d_sigmoid(Z[-1])
     dz_dw = pred[-2]
     delta.append(np.mean(dj_da*da_dz))
-    dj_dw = delta * np.mean(dz_dw, axis=1)
-    return loss, dj_dw
+    dj_dw.append(delta * np.mean(dz_dw, axis=1))
+
+    # Rest of the derivatives
+    for i in range(1, len(Ws)):
+        d = np.dot(weights[-i].T, delta[0])
+        delta.insert(0, d*d_sigmoid(-i-1))
+    for i in range(len(delta)):
+        pass
+    return loss, delta
