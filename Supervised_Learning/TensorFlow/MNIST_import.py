ec2957475e383c75117b007218e24851cd20bf86
Christian Zwinkels-Valero <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2020-01-23 18:26:28 +0100

    Create MNIST_import.py
    Imports the dataset and saves it in a usable file format. Credit goes to: https://stackoverflow.com/a/53570674

---
+++ Supervised_Learning/TensorFlow/MNIST_import.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 23 17:11:43 2020
+
+@author: Christian Zwinkels-Valero
+"""
+
+import gzip
+import numpy as np
+
+img_size = 28  # Images are 28x28 pixels
+num_image = 60000  # Return this amount of images
+
+f = gzip.open("train-images-idx3-ubyte.gz", "r")
+f.read(16)  # Firts 16 bytes are not needed
+buffer = f.read(img_size**2 * num_image)  # Return up to specified image number
+X = np.frombuffer(buffer, dtype=np.uint8())
+X = X.reshape(num_image, img_size, img_size)
+
+o = gzip.open("train-labels-idx1-ubyte.gz", "r")
+o.read(8)
+buffer_o = o.read(num_image)
+y = np.frombuffer(buffer_o, dtype=np.uint8())
+
+np.save("X.npy", X)
+np.save("y.npy", y)
